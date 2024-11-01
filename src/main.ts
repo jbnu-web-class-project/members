991cb71e7bcd3e213cbfcf0c15f5423ec582ce5a
hodu26 <gjdhks1212@gmail.com>
2024-11-01 10:12:42 +0900

    update: update base path
    members -> api/members

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 모든 라우트의 기본 경로를 '/members'로 설정
-  app.setGlobalPrefix('members');
+  // 모든 라우트의 기본 경로를 '/api/members'로 설정
+  app.setGlobalPrefix('api/members');
   
   // CORS 활성화
   app.enableCors({
